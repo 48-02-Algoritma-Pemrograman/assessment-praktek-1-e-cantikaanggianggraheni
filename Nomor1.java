b82962da9f2237d199d5b1c7743227e2eae3cc8b
cantikaanggianggraheni <cantikaanggianggraheni@gmail.com>
2024-10-24 14:23:52 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Nomor1{
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Jumlah anggota keluarga: ");
+        int jumlahAnggota = scanner.nextInt();
+        
+        double tunjanganKesehatan;
+        
+        if (jumlahAnggota <= 3) {
+            tunjanganKesehatan = jumlahAnggota * 100000;
+        } else {
+            tunjanganKesehatan = (3 * 100000) + ((jumlahAnggota - 3) * 50000);
+        }
+        
+        System.out.printf("Besar tunjangan kesehatan" + tunjanganKesehatan);
+        scanner.close();
     }
 }
